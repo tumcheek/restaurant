ca606105377187c6d7c8ed6ffbf6ec080942fc02
Danil <dania.tumanov@gmail.com>
2024-03-19 20:31:43 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module Test{
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens restaurant;
+}
---
+++ src/restaurant/Main.java
@@ -0,0 +1,24 @@
+package restaurant;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, World!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello World App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
